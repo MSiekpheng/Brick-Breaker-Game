e452707635348aefded9ec070fac57dd9a6b4ee3
MSiekpheng <6022010016@camtect.edu.kh>
2023-12-05 14:08:17 +0700

    update

---
+++ GameObject.java
@@ -0,0 +1,46 @@
+public class GameObject {
+    protected int x, y; // Position
+    protected int width, height; // Dimensions
+
+    public GameObject(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    // Getters
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    // Setters
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+}
